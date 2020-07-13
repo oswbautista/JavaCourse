7a2708e6006eeaa0aab057d478babcd99b50b239
Osvaldo Bautista <orbn@yandex.ru>
2020-07-13 18:16:44 +0300

    First commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    /* Если нужны более длинные комментарии, можно каждую строку начинать двумя слешами.
+        Хотя более удобно ограничивать блоки комментариев разделителями */
+    public static void main(String[] args)
+        {
+        System.out.println("Hello, world!");
+        System.out.println("Hello World!");
+        }
+    }
